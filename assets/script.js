19d5eddfb1f96194cd8a61ea16bcb2d45fc08388
Tom hoffman <tomhoffmanco@gmail.com>
2022-04-23 23:45:00 -0400

    Js functionality

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 let questionNumber = 1;
-let timer = 75;
-let timerIntervalId = -1;
+let timer = 75; //set start time of timer
+let timerIntervalId = -1; // interval decreased from timer
 
+//questions to be asked with multiple choice answers displayed
 let questions = [
   {
     text: "Commonly used data types Do not Include:",
@@ -50,27 +51,113 @@ let questions = [
 ];
 
 function startQuiz() {
+  //timer starts and content is displayed
   document.getElementById("boxInfo").style.display = "block";
 
   timerIntervalId = setInterval(() => {
     timer--;
     document.getElementById("time").innerText = timer;
 
+    //end game
     if (timer <= 0) {
       endGame();
     }
   }, 1000);
 }
 
+//end game function
 function endGame() {
-  document.getElementById("questions").innerText = "Finished!";
-  document.getElementById("buttonStyle").style.display = "none";
-  document.getElementById("").style.display = "block";
-  document.getElementById("").style.display = "none";
-
-  // Dsiplay Score
-
+  document.getElementById("question").innerText = "All done!";
+  document.getElementById("buttonsContainer").style.display = "none";
+  document.getElementById("endContainer").style.display = "block";
+  document.getElementById("resultContainer").style.display = "none";
+  //load the score
   document.getElementById("score").innerText =
-    "Your final Score is" + timer + ".";
+    "Your final score is " + timer + ".";
   clearInterval(timerIntervalId);
 }
+
+function choose(choiceNumber) {
+  const isOutOfTime = decideResult(choiceNumber); //true or false
+
+  if (isOutOfTime) {
+    //do something
+  } else {
+    if (questionNumber < questions.length) {
+      navigateToNextQuestion();
+    } else {
+      // no more questions to be answered
+      //end the game
+
+      endGame();
+    }
+  }
+}
+
+//Subtract from timer if wrong ..
+//Change the html of the result ..
+//returns whether or not the game is over due to lack of time
+function decideResult(choiceNumber) {
+  if (questions[questionNumber - 1].correctChoice != choiceNumber) {
+    //incorrect answer
+    timer -= 10;
+
+    document.getElementById("result").innerText = "Wrong!";
+
+    document.getElementById("time").innerText = timer;
+
+    if (timer <= 0) {
+      return true; // out of time quiz over
+    }
+  } else {
+    //correct answer
+    document.getElementById("result").innerText = "Correct!";
+  }
+
+  return false; // time still left quiz continues
+}
+
+function navigateToNextQuestion() {
+  if (questionNumber) questionNumber++;
+
+  const question = questions[questionNumber - 1];
+
+  const questionText = question.text;
+
+  //set and display question text
+  document.getElementById("question").innerText = questionText;
+
+  //set and display button text
+  document.getElementById("choice1").innerText = question.choice1Text;
+  document.getElementById("choice2").innerText = question.choice2Text;
+  document.getElementById("choice3").innerText = question.choice3Text;
+  document.getElementById("choice4").innerText = question.choice4Text;
+}
+
+function submitScore() {
+  //save to local storage
+  //redirect to scores.html
+  const name = document.getElementById("input").value;
+
+  if (name.length <= 0) {
+    console.log("No name was entered.");
+    return;
+  }
+
+  //name is the key,
+  //and the timer score is the value of the item inserted into localStorage
+  window.localStorage.setItem(name, timer);
+  console.log(`User ${name} has saved a score of ${timer}`);
+
+  //could only store score to local storage on first page not redirected url
+
+  window.location.href = "./scores.html";
+}
+
+// display questons and timer
+document.getElementById("time").innerText = timer;
+document.getElementById("question").innerText = questions[0].text;
+document.getElementById("choice1").innerText = questions[0].choice1Text;
+document.getElementById("choice2").innerText = questions[0].choice2Text;
+document.getElementById("choice3").innerText = questions[0].choice3Text;
+document.getElementById("choice4").innerText = questions[0].choice4Text;
